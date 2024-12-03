ea308e802d3195b3adac6d81b005005e3ae41d2f
Rory& <root@rory.gay>
2024-12-03 11:11:22 +0100

    Initial Commit

---
+++ WKQatar/WKQatarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WKQatarApp.swift
+//  WKQatar
+//
+//  Created by RorySys on 03/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct WKQatarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
